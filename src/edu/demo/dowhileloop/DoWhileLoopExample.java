c392138b36c044be92172c41b31ae28f730fad41
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-09 23:02:58 +0100

    do while loop example

--- src/edu/demo/dowhileloop/DoWhileLoopExample.java
+++ src/edu/demo/dowhileloop/DoWhileLoopExample.java
@@ -3,8 +3,30 @@ package edu.demo.dowhileloop;
 public class DoWhileLoopExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int limit = 20;
+		int sum = 0;
+		int i = 1;
+		
+		ReportDoWhileLoopExample report = new ReportDoWhileLoopExample();
 
+		int charLength = 40;
+		char startChar = '[';
+		char delimiterChar = '*';
+		char endChar = ']';
+		report.delimiterLine(charLength, startChar, delimiterChar, endChar);
+		
+		System.out.println("Example 1:");
+		
+		do {
+			sum += i;
+			report.showInsideLoopReport(i, sum);
+			i++;
+		} while (i <= limit);
+		
+		report.showSummaryReport(sum);
+		
+		report.delimiterLine(charLength, startChar, delimiterChar, endChar);
 	}
 
 }
---
+++ src/edu/demo/dowhileloop/ReportDoWhileLoopExample.java
@@ -0,0 +1,32 @@
+package edu.demo.dowhileloop;
+
+public class ReportDoWhileLoopExample {
+	
+	public void showSummaryReport(int summary) {
+		System.out.println("summary = " + summary);
+	}
+	
+	public void showInsideLoopReport(int counter, int tempSummary) {
+		System.out.println("value = " + counter + " sum = " + tempSummary);
+	}
+	
+	public void showInsideLoopReportTwo(int counter, int tempSummary) {
+		String valueText = "value =";
+		String sumText = "sum =";
+		String space = " ";
+		String output = valueText + space + counter + space + sumText + space + tempSummary;
+		System.out.println(output);
+	}
+	
+	public void delimiterLine(int charLength, char startChar, char delimiterChar, char endChar) {
+		
+		System.out.print(startChar);
+		
+		for (int i = 0; i < charLength; i++) {
+			System.out.print(delimiterChar);
+		}
+		
+		System.out.println(endChar);
+	}
+	
+}
