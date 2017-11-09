b1b6fc6bad44beb214b764191b588db54bf85b7a
Sasa Radovanovic <sasaradovanovic888@gmail.com>
2017-11-09 13:46:51 +0100

    test

--- src/edu/TestClass.java
+++ src/edu/TestClass.java
@@ -3,7 +3,7 @@ package edu;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test test");
 		System.out.println("TEST");
 		
 	}
